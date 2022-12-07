0b47e537055ba99030b87a724eb8f05a82a1394b
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-12-07 16:22:57 +0700

    Deleting Node and top function

--- Exe4_087/Program.cs
+++ Exe4_087/Program.cs
@@ -6,28 +6,13 @@ using System.Threading.Tasks;
 
 namespace Exe4_087
 {
-    class Node
-    {
-        public char info;
-        public Node next;
-        public Node(char i, Node n)
-        {
-            info = i;
-            next = n;
-        }
-
-    }
+    
 
     class Stack
     {
         private char[] farhan = new char[105];
 
-        Node top;
-
-        public Stack()
-        {
-            top = null;
-        }
+        
         static void Main(string[] args)
         {
         }
